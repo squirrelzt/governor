3d32b067b5f49226f07b9195e9fa902dcf788d33
squirrelzt <squirrelzt@126.com>
2020-09-22 17:18:05 +0800

    controller添加try-catch

--- src/main/java/com/governer/controller/BatchController.java
+++ src/main/java/com/governer/controller/BatchController.java
@@ -5,7 +5,6 @@ import com.governer.domain.vo.request.ConfigRequestVO;
 import com.governer.domain.vo.response.ConfigResponseVO;
 import com.governer.service.BatchService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
@@ -27,7 +26,12 @@ public class BatchController {
     @ApiOperation(value = "定时-查询", notes = "定时-查询接口")
     public BaseResponse<List<ConfigResponseVO>> config(@RequestParam(required = false) @ApiParam("应用名称") String app_name,
                                                        @RequestParam(required = false) @ApiParam("作业名称") String name) {
-        return batchService.config(app_name, name);
+        try {
+            return batchService.config(app_name, name);
+        }catch (Exception e) {
+            log.error("定时-查询接口失败: ", e.getMessage());
+            return BaseResponse.fail(e.getMessage());
+        }
     }
 
     @PostMapping("config/save")
@@ -39,17 +43,26 @@ public class BatchController {
             log.error("定时-保存接口失败: ", e.getMessage());
             return BaseResponse.fail(e.getMessage());
         }
-
     }
     @PostMapping("config/start")
     @ApiOperation(value = "定时-启用", notes = "定时-启用接口")
     public BaseResponse<Boolean> start(@RequestBody List<ConfigRequestVO> requestVOList) {
-        return batchService.start(requestVOList);
+        try {
+            return batchService.start(requestVOList);
+        }catch (Exception e) {
+            log.error("定时-启用接口失败: ", e.getMessage());
+            return BaseResponse.fail(e.getMessage());
+        }
     }
 
     @PostMapping("config/stop")
     @ApiOperation(value = "定时-停用", notes = "定时-停用接口")
     public BaseResponse<Boolean> stop(@RequestBody List<ConfigRequestVO> requestVOList) {
-        return batchService.stop(requestVOList);
+        try {
+            return batchService.stop(requestVOList);
+        }catch (Exception e) {
+            log.error("定时-停用接口失败: ", e.getMessage());
+            return BaseResponse.fail(e.getMessage());
+        }
     }
 }
