c4ebd338bb5a03d27a8f338995814f50a250d5fe
squirrelzt <squirrelzt@126.com>
2020-09-23 10:04:49 +0800

    controller打印入参

--- src/main/java/com/governer/controller/BatchController.java
+++ src/main/java/com/governer/controller/BatchController.java
@@ -27,45 +27,48 @@ public class BatchController {
     @ApiOperation(value = "定时-查询", notes = "定时-查询接口")
     public BaseResponse<List<ConfigResponseVO>> config(@RequestParam(required = false) @ApiParam("应用名称") String app_name,
                                                        @RequestParam(required = false) @ApiParam("作业名称") String name) {
+        log.info("定时-查询接口入参：app_name={},name={}", app_name, name);
         try {
             return batchService.config(app_name, name);
         }catch (Exception e) {
-            log.error("定时-查询接口失败: ", e.getMessage());
-            return BaseResponse.fail(e.getMessage());
+            log.error("定时-查询接口失败: ", e);
+            return BaseResponse.fail(e.toString());
         }
     }
 
     @PostMapping("config/save")
     @ApiOperation(value = "定时-保存", notes = "定时-保存接口")
     public BaseResponse<Boolean> save(@RequestBody ConfigRequestVO requestVO) {
+        log.info("定时-保存接口入参：requestVO={}", requestVO.toString());
         try {
             log.info(requestVO.toString());
             return batchService.save(requestVO);
         }catch (Exception e) {
-            e.printStackTrace();
-            log.error("定时-保存接口失败: ", e.getMessage());
-            return BaseResponse.fail(e.getMessage());
+            log.error("定时-保存接口失败: ", e);
+            return BaseResponse.fail(e.toString());
         }
     }
     @PostMapping("config/start")
     @ApiOperation(value = "定时-启用", notes = "定时-启用接口")
     public BaseResponse<Boolean> start(@RequestBody SelectedDataRequestVO selectedData) {
+        log.info("定时-启用接口入参：selectedData={}", selectedData.toString());
         try {
             return batchService.start(selectedData.getSelectedData());
         }catch (Exception e) {
-            log.error("定时-启用接口失败: ", e.getMessage());
-            return BaseResponse.fail(e.getMessage());
+            log.error("定时-启用接口失败: ", e);
+            return BaseResponse.fail(e.toString());
         }
     }
 
     @PostMapping("config/stop")
     @ApiOperation(value = "定时-停用", notes = "定时-停用接口")
     public BaseResponse<Boolean> stop(@RequestBody SelectedDataRequestVO selectedData) {
+        log.info("定时-停用接口入参：selectedData={}", selectedData.toString());
         try {
             return batchService.stop(selectedData.getSelectedData());
         }catch (Exception e) {
-            log.error("定时-停用接口失败: ", e.getMessage());
-            return BaseResponse.fail(e.getMessage());
+            log.error("定时-停用接口失败: ", e);
+            return BaseResponse.fail(e.toString());
         }
     }
 }
