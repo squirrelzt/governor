8f348f66ab9c131a143ea58912f95ab56540d82f
squirrelzt <squirrelzt@126.com>
2020-09-24 09:51:19 +0800

    design url修改

--- src/main/java/com/governer/controller/DesignController.java
+++ src/main/java/com/governer/controller/DesignController.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("design")
+@RequestMapping("batch/design")
 @Api(value = "批量接口", produces = "批量接口服务")
 public class DesignController {
 
